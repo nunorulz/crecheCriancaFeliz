6348fd48acf25cfe80d360e03f55595ff2b7a21b
Nuno Rocha <nunorulz@gmail.com>
2024-01-09 15:39:14 -0300

    Conclusão: Index, rodapé e menu, responsivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        roxo: '#622363',
+        rosaLeve: '#E8B1D1',
+        rosa: '#C73C82',
+        rosaHover: '#9F1D60',
+        verde: '#71DD3F',
+        verdeHover: '#47A01D',
+        cinzaNuvem: '#f6f6f6',
+        turqueza: '#236687',
+        ciano: '#5DDCE3',
+        amarelo: '#FAC500',
+      },
+    },
   },
   plugins: [],
-}
-
+};
